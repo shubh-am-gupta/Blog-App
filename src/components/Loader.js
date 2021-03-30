1bd6c01d9178e9dfbec369d93dd10f9f0a1ba76e
shubh-am-gupta <saishubhamgupta345@gmail.com>
2021-03-30 23:23:25 +0530

    updated

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import spinner from "./spinner.gif";
 
 const Loader = () => {
   return (
-    <div className="loader">
+    <div className="loader" style={{ textAlign: "center" }}>
       <img src={spinner} alt="Loading" />
     </div>
   );
